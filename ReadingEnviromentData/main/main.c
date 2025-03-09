a58526acdc579cf3b9d157e8acff5ee3dca59596
RyanWWU <rootr@wwu.edu>
2025-03-09 16:54:27 -0700

    Moved NVS initialization to main

--- ReadingEnviromentData/main/main.c
+++ ReadingEnviromentData/main/main.c
@@ -1,36 +1,34 @@
 // Ryan Root
 
 #include "stdio.h"
+#include "nvs_flash.h"
 #include "ReadPhotoResistor.h"
 #include "client.h"
 #include "wireless.h"
 
+void init_NVS() {
+    esp_err_t ret = nvs_flash_init();
+    if (ret == ESP_ERR_NVS_NO_FREE_PAGES || ret == ESP_ERR_NVS_NEW_VERSION_FOUND) {
+        ESP_ERROR_CHECK(nvs_flash_erase());
+        ret = nvs_flash_init();
+    }
+    ESP_ERROR_CHECK(ret);
+}
+
 void app_main(void) {
+    init_NVS();
+
     // Attempt connection
-    if (!LwIP_WiFi_Init()) {
-        printf("Failed to init wifi\n");
-    } else {
-        printf("Sucessfully init wifi\n");
-    }
-
-    if (!WiFi_Config()) {
-        printf("Failed to configure wifi\n");
-    } else {
-        printf("Sucessfully configured wifi\n");
-    }
-
-    if (!WiFi_Start()) {
-        printf("Failed to start wifi\n");
-    } else {
-        printf("Sucessfully started wifi\n");
-    }
+    LwIP_WiFi_Init();
 
     // Delay 5 seconds
     vTaskDelay(pdMS_TO_TICKS(5000));
     print_connection_info();
 
+    // Read analog data 25 times
     config_adc();
     endless_read();
 
+    // Stop and flush our connection
     WiFi_Deinit();
 }
