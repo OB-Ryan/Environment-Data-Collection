c07c7df84ee043aafb8eb7e2b027bdeff92793ac
RyanWWU <rootr@wwu.edu>
2025-03-10 18:47:25 -0700

    Implemented first version of MQTT client for ESP32 - toy example for now. Uses an EventGroup to esnsure connection before first event

--- ReadingEnviromentData/main/client.c
+++ ReadingEnviromentData/main/client.c
@@ -3,17 +3,103 @@
 
 #include "client.h"
 
-void start_mqtt () {
-    /*
-    const esp_mqtt_client_config_t mqtt_cfg = {
-        .broker.address.uri = BROKER_ADR
-    };
-    esp_mqtt_client_handle_t client = esp_mqtt_client_init(&mqtt_cfg);
-    esp_mqtt_client_register_event(client, ESP_EVENT_ANY_ID, mqtt_event_handler, client);
-    esp_mqtt_client_start(client);
-    */
+static const char* TAG = "MQTT";
+static esp_mqtt_client_handle_t client;
+static int curr_reconnect_num = 0;
+EventGroupHandle_t mqtt_event_group;
+
+static void mqtt_event_handler(void* args, esp_event_base_t event_base, int32_t event_id, void* event_data) {
+    esp_mqtt_event_handle_t event = (esp_mqtt_event_handle_t)event_data;
+
+    switch (event_id) {
+        case MQTT_EVENT_CONNECTED:
+            ESP_LOGI(TAG, "Client has connected\n");
+            // QoS 0; Fire and Forget
+            esp_mqtt_client_subscribe(client, TOPIC, 0);
+            ESP_LOGI(TAG, "Cient has subscribed to topic %s\n", TOPIC);
+            break;
+        case MQTT_EVENT_DISCONNECTED:
+            if (curr_reconnect_num < MAX_RECONNECT_ATTEMPTS) {
+                ESP_LOGW(TAG, "MQTT disconnected. Attempting to reconnect\n");
+                esp_mqtt_client_subscribe(client, TOPIC, 0);
+                curr_reconnect_num++;
+            } else {
+                xEventGroupSetBits(mqtt_event_group, MQTT_FAIL_BIT);
+                ESP_LOGE(TAG, "MQTT connection failed.\n");
+            }
+            break;
+        case MQTT_EVENT_SUBSCRIBED:
+            ESP_LOGI(TAG, "Client has subscribed to a topic\n");
+            publish_message_mqtt();
+            break;
+        case MQTT_EVENT_UNSUBSCRIBED:
+            ESP_LOGI(TAG, "Client has unsubscribed from a topic\n");
+            break;
+        case MQTT_EVENT_PUBLISHED:
+            ESP_LOGI(TAG, "Client published a message.\n");
+            break;
+        case MQTT_EVENT_ERROR:
+            ESP_LOGE(TAG, "MQTT EVENT ERROR!\n");
+            break;
+        case MQTT_EVENT_DATA:
+            ESP_LOGI(TAG, "Received event data\n");
+            break;
+        default:
+            ESP_LOGW(TAG, "MQTT Event %ld left undandled...!\n", event_id);
+            break;
+    }
 }
 
-void start_mqtt();
-void send_message();
-void end_mqtt();
+void init_mqtt () {
+    mqtt_event_group = xEventGroupCreate();
+
+    const esp_mqtt_client_config_t mqtt_cfg = {
+        .broker.address.uri = BROKER_URI
+    };
+
+    client = esp_mqtt_client_init(&mqtt_cfg);
+    ESP_ERROR_CHECK(
+        esp_mqtt_client_register_event(
+            client,
+            ESP_EVENT_ANY_ID,
+            mqtt_event_handler,
+            client
+        )
+    );
+    ESP_ERROR_CHECK(esp_mqtt_client_start(client));
+
+    // Wait until mqtt connection is established, or failed maximum number of allowed attempts
+    EventBits_t event_bits = xEventGroupWaitBits(mqtt_event_group,
+        MQTT_CONNECTED_BIT | MQTT_FAIL_BIT,
+        pdFALSE,
+        pdFALSE,
+        portMAX_DELAY
+    );
+
+    // Check the status of our EventGroup
+    if (event_bits & MQTT_CONNECTED_BIT) {
+        ESP_LOGI(TAG, "Connected to %s\n", BROKER_ADR);
+    } else if (event_bits & MQTT_FAIL_BIT) {
+        ESP_LOGI(TAG, "Failed to connect to %s\n", BROKER_ADR);
+    } else {
+        ESP_LOGE(TAG, "Error with event!");
+    }
+}
+
+void publish_message_mqtt() {
+    /*
+    *   PUBLISH TEST:
+    *       - Test string
+    *       - QoS 0, Fire and Forget
+    *       - len: 0 (is calcuated from data string)
+    *       - retain 1: Broker retains
+    */
+    esp_mqtt_client_publish(
+        client,
+        TOPIC,
+        "Hello, message string!\n",
+        0,
+        0,
+        1
+    );
+}
--- ReadingEnviromentData/main/client.h
+++ ReadingEnviromentData/main/client.h
@@ -2,10 +2,23 @@
 // MQTT Client Functions
 
 #include "mqtt_client.h"
+#include "esp_log.h"
+#include "esp_event.h"
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "freertos/event_groups.h"
 
-#define BROKER_ADR "BrokerAddress"
+#define BROKER_ADR              ""
+#define BROKER_URI              ""
+#define TOPIC                   "EnviromentData"
+#define TCP_PORT                1883
+#define TLS_PORT                8883
+#define MQTT_CONNECTED_BIT      BIT0
+#define MQTT_FAIL_BIT           BIT1
+#define MAX_RECONNECT_ATTEMPTS  10
 
-void mqtt_event_handler();
-void start_mqtt();
-void send_message();
-void end_mqtt();
+extern EventGroupHandle_t mqtt_event_group;
+
+static void mqtt_event_handler(void* args, esp_event_base_t event_base, int32_t event_id, void* event_data);
+void init_mqtt();
+void publish_message_mqtt();
