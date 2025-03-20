7541c2b1bcad1429768a8d834680b701b7f6f299
RyanWWU <rootr@wwu.edu>
2025-03-19 23:22:02 -0700

    Changed buffer copy of analog read in main to use snprintf; For buffer saftey

--- ReadingEnviromentData/main/main.c
+++ ReadingEnviromentData/main/main.c
@@ -26,10 +26,8 @@ void app_main(void) {
     ESP_LOGI(TAG, "Initializing and Starting WiFi\n");
     LwIP_WiFi_Init();
 
-    // Delay 5 seconds
-    // Delay 5 seconds
     printf("Connecting to WiFi\n");
-    for (int i = 1; i <= 5; i++) {
+    for (int i = 1; i <= 3; i++) {
         printf("%d...\n", i);
         vTaskDelay(pdMS_TO_TICKS(1000));
     }
@@ -39,20 +37,20 @@ void app_main(void) {
     ESP_LOGI(TAG, "Initializing and Starting connection to MQTT Broker\n");
     init_mqtt();
 
-    // Delay 5 seconds
     printf("Connecting to Broker\n");
-    for (int i = 1; i <= 5; i++) {
+    for (int i = 1; i <= 3; i++) {
         printf("%d...\n", i);
         vTaskDelay(pdMS_TO_TICKS(1000));
     }
 
     // Read analog data froever, publish each reading
-    ESP_LOGI(TAG, "Starting Analog Data Collection\n");
     config_adc();
+    publish_message_mqtt("Beginning Endless (Photo Resistor) Analog Read:");
+    ESP_LOGI(TAG, "Starting Analog Data Collection\n");
     char analog_string_buf[20];
     while (1) {
         int analog_read = read_analog();
-        sprintf(analog_string_buf, "%d", analog_read);
+        snprintf(analog_string_buf, sizeof(analog_string_buf), "%d", analog_read);
         publish_message_mqtt(analog_string_buf);
         vTaskDelay(pdMS_TO_TICKS(1000));
     }
